1b6c9d78dd85790584736458e9782125c4f9b5fb
Aubrey Howell <simplyaubs@gmail.com>
2014-03-27 14:32:55 -0600

    Setup gems, specs, and config files
    Signed-off-by: Paul Wenig <pwenig@gmail.com>

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra/base'
+class App < Sinatra::Application
+
+  get '/' do
+    erb :index
+  end
+
+
+end
---
+++ spec/url_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+require 'capybara/rspec'
+require_relative '../app'
+
+Capybara.app = App
