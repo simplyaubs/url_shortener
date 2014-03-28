622058deccf5d0afef5fda052bb47507fa0eb92d
Aubrey Howell <simplyaubs@gmail.com>
2014-03-28 13:30:14 -0600

    [68391768] Made layout responsive and centered submit button
    Signed-off-by: Paul Wenig <pwenig@gmail.com>

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -3,3 +3,13 @@ require 'capybara/rspec'
 require_relative '../app'
 
 Capybara.app = App
+
+feature 'url shortener' do
+
+  scenario 'shortening url' do
+
+    visit ('/')
+    expect(page).to have_title "URL Shortener"
+    click_on("Shorten")
+  end
+end
