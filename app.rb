cd35ee97bb7ccdb27cf831ce2739008bf5b242b6
Aubrey Howell <simplyaubs@gmail.com>
2014-03-28 16:43:44 -0600

    [68391770] User can see and shorten original URL
    Signed-off-by: Paul Wenig <pwenig@gmail.com>

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require 'sinatra/base'
 class App < Sinatra::Application
 
+  ORIGINAL_URL = []
+  SHORT_URL = []
+
   get '/' do
     erb :index
   end
 
+  post '/' do
+    ORIGINAL_URL << params[:input_url]
+    ORIGINAL_URL.each_with_index do |url, index|
+      SHORT_URL << "http://www.staging-url-shortener.herokuapp.com/#{index + 1}"
+    end
+    redirect '/result'
+  end
+
+  get '/result' do
+    erb :result, :locals => {:url => ORIGINAL_URL, :new_url => SHORT_URL}
+
+  end
 
 end
+
--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -10,6 +10,10 @@ feature 'url shortener' do
 
     visit ('/')
     expect(page).to have_title "URL Shortener"
+    fill_in 'input_url', with: 'http://tutorials.gschool.it/http_with_sinatra_spike'
     click_on("Shorten")
+    expect(page).to have_content "http://tutorials.gschool.it/http_with_sinatra_spike"
+    expect(page).to have_content "http://production-url-shortener.herokuapp.com/1"
+
   end
 end
